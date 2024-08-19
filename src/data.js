fef71c2c1f929178b1c81413f8bf893114545ff8
GuhaPranav09 <yg.pranav@gmail.com>
2024-08-19 13:33:06 +0530

    Rollback ;

--- src/data.js
+++ src/data.js
@@ -1,150 +1,6 @@
-// // FileName: data.js 
-  
-// import { v4 as uuidv4 } from "uuid"; 
-// function chillHop() { 
-//   return [ 
-//     { 
-//       name: "Satisfya",
-//       artist: "Imran Khan",
-//       cover:
-//           "https://images.genius.com/e524761aa1d93841d311ba961e0cb278.1000x1000x1.jpg",
-      
-//       audio: "https://aac.saavncdn.com/931/423b9e98db7f5ad575ee19e383d4c30e_320.mp4",
-//       color: "purple",
-//       id: uuidv4(),
-//       active:true,
-//     }, 
-//     { 
-//       name: "Halamathi Habibo", 
-//       cover: 
-//         "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgzYetmgd8bYuMli8uifmNpzWJ6hw5sNCBwctxC_R3Qxei-99VL9VWluPge2JbVS35UipcL9OJqLZC7O0WiSVzTWJ8bZFGaLok6OG4ep2EKYDsMUL-QpmYWJG1Ps5snuYb2TBBvsv5ExRUzm5fH136XLcesUlX-EA34NJLlJXEwF4V_62kd3dlPWPiwkg/s3264/editing%20world%20(5).png", 
-//       artist: "Anirudh", 
-//       audio: 
-//         "https://aac.saavncdn.com/510/9d96fc7ddd4ffadb745f25aed86f7a4e_160.mp4", 
-//       color: ["#EF8EA9", "#ab417f"], 
-//       id: uuidv4(), 
-//       active: false, 
-//     }, 
-
-//     { 
-//       name: "Jawan Title Track", 
-//       cover: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/4708fbf8-4748-4d11-8fd3-2e4ea4e79c11/dg8rdvc-3fa0fef4-1c19-4f50-a10b-29e0f858608e.png", 
-//       artist: "Anirudh", 
-//       audio: 
-// "https://media.geeksforgeeks.org/wp-content/uploads/20231004185212/Jawan-Prevue-Theme.mp3", 
-//       color: ["#CD607D", "#c94043"], 
-//       id: uuidv4(), 
-//       active: false, 
-//     }, 
-//     { 
-//       name: "Oh Mahiye", 
-//       cover: 
-// "https://c.saavncdn.com/566/Manamey-Original-Motion-Picture-Soundtrack-Telugu-2024-20240606210559-500x500.jpg", 
-//       artist: "Hesham Abdul Wahab, Harika Narayan", 
-//       audio: 
-// "https://aac.saavncdn.com/566/9457dca66f48b04a9cc402c4da4a03fe_160.mp4", 
-//       color: ["#EF8EA9", "#ab417f"], 
-//       id: uuidv4(), 
-//       active: false, 
-//     }, 
-//     { 
-//       name: "Dernier Danse", 
-//       cover: 
-// "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7f98720f-ce18-406b-a434-acc46ca53226/d782fa8-c0058b75-1285-4816-9306-63d58bb07733.png/v1/fill/w_960,h_804/indila_by_navykatycat_d782fa8-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODA0IiwicGF0aCI6IlwvZlwvN2Y5ODcyMGYtY2UxOC00MDZiLWE0MzQtYWNjNDZjYTUzMjI2XC9kNzgyZmE4LWMwMDU4Yjc1LTEyODUtNDgxNi05MzA2LTYzZDU4YmIwNzczMy5wbmciLCJ3aWR0aCI6Ijw9OTYwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.51nsqcFEcMaFsT_R-f9y5mdVymxiovBgHz5duCoXzIw", 
-//       artist: "Indila", 
-//       audio: 
-// "https://aac.saavncdn.com/709/f961caafb9ab1e0d9f1c6957c7b143ca_160.mp4", 
-//       color: ["#CD607D", "#c94043"], 
-//       id: uuidv4(), 
-//       active: false, 
-//     }, 
-//     { 
-//       name: "Nenjukkul Peidhidum", 
-//       cover: 
-// "https://i.pinimg.com/564x/c1/78/57/c17857aadf2060eb05f6adeb39efdf20.jpg", 
-//       artist: "Harris Jayaraj", 
-//       audio: 
-// "https://aac.saavncdn.com/635/7482341659875f67c658dc9a7d7a2ad4_160.mp4", 
-
-//       color: ["#205950", "#2ab3bf"], 
-//       id: uuidv4(), 
-//       active: false, 
-//     }, 
-//     { 
-//       name: "Perfect", 
-//       cover: 
-// "https://upload.wikimedia.org/wikipedia/en/d/d7/Ed_Sheeran_Perfect.png", 
-//       artist: "Ed Sheeran", 
-//       audio: 
-// "https://aac.saavncdn.com/415/7624be6e5b093d0e3f7ef06d541a69fa_160.mp4", 
-//       color: ["#f3a683", "#f19066"], 
-//       id: uuidv4(), 
-//       active: false, 
-//     }, 
-//     { 
-//       name: "Believer", 
-//       cover: 
-// "https://upload.wikimedia.org/wikipedia/en/e/e7/Imagine_Dragons_Believer.jpg", 
-//       artist: "Imagine Dragons", 
-//       audio: 
-// "https://aac.saavncdn.com/809/bf2b3c168b0216885c580fa50f40d616_160.mp4", 
-//       color: ["#ffeaa7", "#fdcb6e"], 
-//       id: uuidv4(), 
-//       active: false, 
-//     }, 
-//     { 
-//       name: "Shape of You", 
-//       cover: 
-// "https://upload.wikimedia.org/wikipedia/en/4/45/Shape_of_You_%28Official_Single_Cover%29_by_Ed_Sheeran.png", 
-//       artist: "Ed Sheeran", 
-//       audio: 
-// "https://aac.saavncdn.com/739/7a5b784c7c3d6aeecc6a7edc1f66a1ca_160.mp4", 
-//       color: ["#6c5ce7", "#74b9ff"], 
-//       id: uuidv4(), 
-//       active: false, 
-//     }, 
-//     { 
-//       name: "Senorita", 
-//       cover: 
-// "https://upload.wikimedia.org/wikipedia/en/8/8f/Shawn_Mendes_and_Camila_Cabello_-_Senorita.png", 
-//       artist: "Shawn Mendes, Camila Cabello", 
-//       audio: 
-// "https://aac.saavncdn.com/249/98e52a93ff0a4e57c135d848ebc7aeb6_160.mp4", 
-//       color: ["#fd79a8", "#e84393"], 
-//       id: uuidv4(), 
-//       active: false, 
-//     }, 
-//     { 
-//       name: "Lover", 
-//       cover: 
-// "https://upload.wikimedia.org/wikipedia/en/c/cd/Taylor_Swift_-_Lover.png", 
-//       artist: "Taylor Swift", 
-//       audio: 
-// "https://aac.saavncdn.com/897/1f0fa22d7bb7a4b8197f2f3f5f283a12_160.mp4", 
-//       color: ["#ff7675", "#d63031"], 
-//       id: uuidv4(), 
-//       active: false, 
-//     }, 
-//     { 
-//       name: "Blinding Lights", 
-//       cover: 
-// "https://upload.wikimedia.org/wikipedia/en/e/e6/The_Weeknd_-_Blinding_Lights.png", 
-//       artist: "The Weeknd", 
-//       audio: 
-// "https://aac.saavncdn.com/850/1d1204b18c3fe6a7a83c6063f5f2a331_160.mp4", 
-//       color: ["#00b894", "#00cec9"], 
-//       id: uuidv4(), 
-//       active: false, 
-//     },
-//   ]; 
-// } 
-  
-// export default chillHop; 
-
 // FileName: data.js 
-
+  
 import { v4 as uuidv4 } from "uuid"; 
-
 function chillHop() { 
   return [ 
     { 
@@ -152,7 +8,8 @@ function chillHop() {
       artist: "Imran Khan",
       cover:
           "https://images.genius.com/e524761aa1d93841d311ba961e0cb278.1000x1000x1.jpg",
-      audio: "https://pagalsong.in/uploads/systemuploads/mp3/Satisfya%20-%20Imran%20Khan/Satisfya%20-%20Imran%20Khan%20128%20Kbps.mp3",
+      
+      audio: "https://aac.saavncdn.com/931/423b9e98db7f5ad575ee19e383d4c30e_320.mp4",
       color: "purple",
       id: uuidv4(),
       active:true,
@@ -163,17 +20,18 @@ function chillHop() {
         "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgzYetmgd8bYuMli8uifmNpzWJ6hw5sNCBwctxC_R3Qxei-99VL9VWluPge2JbVS35UipcL9OJqLZC7O0WiSVzTWJ8bZFGaLok6OG4ep2EKYDsMUL-QpmYWJG1Ps5snuYb2TBBvsv5ExRUzm5fH136XLcesUlX-EA34NJLlJXEwF4V_62kd3dlPWPiwkg/s3264/editing%20world%20(5).png", 
       artist: "Anirudh", 
       audio: 
-        "https://pagalsong.in/uploads/systemuploads/mp3/Halamithi%20Habibo%20-%20Beast%20128%20Kbps.mp3", 
+        "https://aac.saavncdn.com/510/9d96fc7ddd4ffadb745f25aed86f7a4e_160.mp4", 
       color: ["#EF8EA9", "#ab417f"], 
       id: uuidv4(), 
       active: false, 
     }, 
+
     { 
       name: "Jawan Title Track", 
       cover: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/4708fbf8-4748-4d11-8fd3-2e4ea4e79c11/dg8rdvc-3fa0fef4-1c19-4f50-a10b-29e0f858608e.png", 
       artist: "Anirudh", 
       audio: 
-"https://pagalsong.in/uploads/systemuploads/mp3/Jawan%20Prevue%20Theme%20128%20Kbps.mp3", 
+"https://media.geeksforgeeks.org/wp-content/uploads/20231004185212/Jawan-Prevue-Theme.mp3", 
       color: ["#CD607D", "#c94043"], 
       id: uuidv4(), 
       active: false, 
@@ -184,7 +42,7 @@ function chillHop() {
 "https://c.saavncdn.com/566/Manamey-Original-Motion-Picture-Soundtrack-Telugu-2024-20240606210559-500x500.jpg", 
       artist: "Hesham Abdul Wahab, Harika Narayan", 
       audio: 
-"https://pagalsong.in/uploads/systemuploads/mp3/Oh%20Mahiye%20-%20Manamey%20128%20Kbps.mp3", 
+"https://aac.saavncdn.com/566/9457dca66f48b04a9cc402c4da4a03fe_160.mp4", 
       color: ["#EF8EA9", "#ab417f"], 
       id: uuidv4(), 
       active: false, 
@@ -192,10 +50,10 @@ function chillHop() {
     { 
       name: "Dernier Danse", 
       cover: 
-"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7f98720f-ce18-406b-a434-acc46ca53226/d782fa8-c0058b75-1285-4816-9306-63d58bb07733.png/v1/fill/w_960,h_804/indila_by_navykatycat_d782fa8-fullview.png", 
+"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7f98720f-ce18-406b-a434-acc46ca53226/d782fa8-c0058b75-1285-4816-9306-63d58bb07733.png/v1/fill/w_960,h_804/indila_by_navykatycat_d782fa8-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODA0IiwicGF0aCI6IlwvZlwvN2Y5ODcyMGYtY2UxOC00MDZiLWE0MzQtYWNjNDZjYTUzMjI2XC9kNzgyZmE4LWMwMDU4Yjc1LTEyODUtNDgxNi05MzA2LTYzZDU4YmIwNzczMy5wbmciLCJ3aWR0aCI6Ijw9OTYwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.51nsqcFEcMaFsT_R-f9y5mdVymxiovBgHz5duCoXzIw", 
       artist: "Indila", 
       audio: 
-"https://pagalsong.in/uploads/systemuploads/mp3/Dernier%20Danse%20-%20Indila%20128%20Kbps.mp3", 
+"https://aac.saavncdn.com/709/f961caafb9ab1e0d9f1c6957c7b143ca_160.mp4", 
       color: ["#CD607D", "#c94043"], 
       id: uuidv4(), 
       active: false, 
@@ -206,79 +64,13 @@ function chillHop() {
 "https://i.pinimg.com/564x/c1/78/57/c17857aadf2060eb05f6adeb39efdf20.jpg", 
       artist: "Harris Jayaraj", 
       audio: 
-"https://pagalsong.in/uploads/systemuploads/mp3/Nenjukkul%20Peidhidum%20-%20Harris%20Jayaraj%20128%20Kbps.mp3", 
+"https://aac.saavncdn.com/635/7482341659875f67c658dc9a7d7a2ad4_160.mp4", 
+
       color: ["#205950", "#2ab3bf"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    // New songs added below
-    { 
-      name: "Perfect", 
-      cover: 
-"https://upload.wikimedia.org/wikipedia/en/d/d7/Ed_Sheeran_Perfect.png", 
-      artist: "Ed Sheeran", 
-      audio: 
-"https://pagalsong.in/uploads/systemuploads/mp3/Perfect%20-%20Ed%20Sheeran%20128%20Kbps.mp3", 
-      color: ["#f3a683", "#f19066"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Believer", 
-      cover: 
-"https://upload.wikimedia.org/wikipedia/en/e/e7/Imagine_Dragons_Believer.jpg", 
-      artist: "Imagine Dragons", 
-      audio: 
-"https://pagalsong.in/uploads/systemuploads/mp3/Believer%20-%20Imagine%20Dragons%20128%20Kbps.mp3", 
-      color: ["#ffeaa7", "#fdcb6e"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Shape of You", 
-      cover: 
-"https://upload.wikimedia.org/wikipedia/en/4/45/Shape_of_You_%28Official_Single_Cover%29_by_Ed_Sheeran.png", 
-      artist: "Ed Sheeran", 
-      audio: 
-"https://pagalsong.in/uploads/systemuploads/mp3/Shape%20of%20You%20-%20Ed%20Sheeran%20128%20Kbps.mp3", 
-      color: ["#6c5ce7", "#74b9ff"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Senorita", 
-      cover: 
-"https://upload.wikimedia.org/wikipedia/en/8/8f/Shawn_Mendes_and_Camila_Cabello_-_Senorita.png", 
-      artist: "Shawn Mendes, Camila Cabello", 
-      audio: 
-"https://pagalsong.in/uploads/systemuploads/mp3/Senorita%20-%20Shawn%20Mendes%20%26%20Camila%20Cabello%20128%20Kbps.mp3", 
-      color: ["#fd79a8", "#e84393"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Lover", 
-      cover: 
-"https://upload.wikimedia.org/wikipedia/en/c/cd/Taylor_Swift_-_Lover.png", 
-      artist: "Taylor Swift", 
-      audio: 
-"https://pagalsong.in/uploads/systemuploads/mp3/Lover%20-%20Taylor%20Swift%20128%20Kbps.mp3", 
-      color: ["#ff7675", "#d63031"], 
-      id: uuidv4(), 
-      active: false, 
-    }, 
-    { 
-      name: "Blinding Lights", 
-      cover: 
-"https://upload.wikimedia.org/wikipedia/en/e/e6/The_Weeknd_-_Blinding_Lights.png", 
-      artist: "The Weeknd", 
-      audio: 
-"https://pagalsong.in/uploads/systemuploads/mp3/Blinding%20Lights%20-%20The%20Weeknd%20128%20Kbps.mp3", 
-      color: ["#00b894", "#00cec9"], 
       id: uuidv4(), 
       active: false, 
     }, 
   ]; 
 } 
-
-export default chillHop;
+  
+export default chillHop; 
